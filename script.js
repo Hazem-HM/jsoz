b30313904449b94515d4b63769c71432908cfc00
Hazem Mohammad <hazem743@hotmail.com>
2023-07-28 11:45:17 +0200

    testing 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let js = "fun";
-if(js === 'fun') console.log('yes it is fun');
+if (js === "fun") console.log("yes it is fun");
 let h1 = document.write('<h1 id="fofo">hello</h1>');
-document.getElementById('fofo').style.color = "red"
+document.getElementById("fofo").style.color = "blue";
