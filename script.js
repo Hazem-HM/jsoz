2769d3eb14d7bf89108ca0febeb437f98412f6b6
Hazem Mohammad <hazem743@hotmail.com>
2023-07-30 12:31:30 +0200

    end of ex 38

--- script.js
+++ script.js
@@ -93,3 +93,110 @@
 // // Write Your Code Here
 
 // console.log(--points - true); // 8;
+// let myVar = "100.56789 Views";
+
+// console.log(Number.parseInt(myVar)); // 100
+// console.log(Number.parseFloat(myVar).toFixed(2)); // 100
+// // console.log(); // 100.57
+
+// let num = 10;
+
+// // console.log((Number.isInteger(num) + true)); // 2
+// let flt = 10.4;
+
+// console.log(Number(flt.toFixed())); // 10
+// console.log(Math.floor(flt)); // 10
+// console.log(Math.trunc(flt)); // 10
+// console.log(Number.parseInt(flt)); // 10
+// // console.log(Math.round(flt)); // 10
+
+// console.log(Math.trunc((Math.random() * 4))); // 0 || 1 || 2 || 3 || 4
+
+// let userName = "Elzero";
+// console.log(userName.charAt(0).toLowerCase()); // e
+// console.log(userName[0].toLowerCase()); // e
+// console.log(userName.slice(0,1).toLowerCase()); // e
+// console.log(userName.split('')[0].toLowerCase()); // e
+// console.log(userName.substring(0,1).toLowerCase()); // e
+// console.log(userName.substr(0,1).toLowerCase()); // 
+// console.log(userName.substring(0,1).toLowerCase().repeat(3)); // eee
+
+// let word = "Elzero";
+// let letterZ = "z";
+// let letterE = "e";
+// let letterO = "O";
+
+// console.log(word.includes(letterZ)); // True
+// console.log(word.startsWith(letterE.toUpperCase())); // True
+// console.log(word.endsWith(letterO.toLowerCase())); // True
+
+// console.log(100 == "100"); // true
+// console.log( 100 !=  1000); // true
+// console.log(110 > 100 > 10 < 20); // true
+// console.log(-10 == "-10"); // true
+// console.log(! -50 >= +"-40"); // true
+// console.log(! 10 <= -"-40"); // true
+// console.log(+ "10" >= 10); // true
+// // console.log(! 20 == false); // true
+
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log("Your Code Here"); // true
+// console.log("Your Code Here"); // true
+// console.log("Your Code Here"); // true
+// console.log("Your Code Here"); // true
+// console.log("Your Code Here"); // true
+// console.log("Your Code Here"); // true
+
+// Test Case 1
+// let num = 110; // "009"
+
+// if ( num < 10){
+//   console.log(`00${num}`);
+// }else if(num <100 && num > 10){
+//   console.log(`0${num}`);
+// }else if(num >= 100){
+//   console.log(num);
+// }
+
+// Test Case 2
+// let num = 20; // "020"
+
+// Test Case 3
+// // let num = 110; // "110"
+
+// let num1 = 9;
+// let str = "9";
+// let str2 = "20";
+
+// // Output
+// "{num1} Is The Same Value As {str}"
+// "{num1} Is The Same Value As {str} But Not The Same Type"
+// ""
+// ""
+
+// if(num1 == str){
+//   console.log(`{num1} is the same value as {str}`);
+// }
+
+// if(num1 == str && typeof num1 != typeof str){
+
+// console.log( `{num1} Is The Same Value As {str} But Not The Same Type`);
+// }
+
+// if(num1 != str2 && typeof num1 !== typeof str2){
+//   console.log(` {num1} Is Not The Same Value Or The Same Type As {str2}`);
+// }
+// if(str != str2 && typeof str === typeof str2){
+//   console.log(` {str} Is The Same Type As {str2} But Not The Same Value`);
+// }
+
+let num1 = 10;
+let num2 = 30;
+let num3 = "30";
+
+// Needed Output
+"30 Is Larger Than 10 And Type string Not The Same Type As number"
+"30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"
+"{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}"
